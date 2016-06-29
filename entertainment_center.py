d1ae66f0d1cbd38b7710d3827e0572aaa75a1695
Steven Guo <mguo@aisle411.com>
2016-06-29 15:53:30 -0500

    no message

--- entertainment_center.py
+++ entertainment_center.py
@@ -2,7 +2,7 @@ import fresh_tomatoes
 import media
 
 #instantiate a movie instance
-warcraft = media.Movie("Warcraft", "2h 30m",
+warcraft = media.Movie("Warcraft", "150min",
                        "The film portrays the origin story of the initial encounters"
                        " between the humans and the orcs, with an emphasis upon both the Alliance's"
                        " and Horde's sides of their conflict. Featuring characters such as Durotan and Lothar,"
@@ -10,8 +10,14 @@ warcraft = media.Movie("Warcraft", "2h 30m",
                        "https://upload.wikimedia.org/wikipedia/en/5/56/Warcraft_Teaser_Poster.jpg",
                        "https://www.youtube.com/watch?v=RhFMIRuHAL4")
 
+dory = media.Movie("Finding Dory", "97min",
+                   "The friendly-but-forgetful blue tang fish begins a search for her long-lost parents,"
+                   " and everyone learns a few things about the real meaning of family along the way.",
+                   "http://ia.media-imdb.com/images/M/MV5BNzg4MjM2NDQ4MV5BMl5BanBnXkFtZTgwMzk3MTgyODE@._V1_SY1000_CR0,0,674,1000_AL_.jpg",
+                   "https://www.youtube.com/watch?v=dLIy1K8kJPo")
+
 #construct a list of movies
-movies = [warcraft, warcraft, warcraft, warcraft, warcraft, warcraft]
+movies = [warcraft, warcraft, warcraft, dory, dory, dory]
 
 #construct the page with data above
 fresh_tomatoes.open_movies_page(movies)
